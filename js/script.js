4c2bf73771c627f3e62d11dcc7687dc254280cf8
DociIva <doci.iva@hotmail.com>
2021-04-21 18:20:55 +0200

    add-img-and-scipt

--- js/script.js
+++ js/script.js
@@ -4,10 +4,29 @@
 const app = new Vue({
     el: '#app',
     data: {       
-        photos: [  // arr per le img
-            
+        photos: [  
+            './image/tiger-jpg.jpg',
+            './image/cervo.jpg',
+            './image/gatto.jpg',
+            './image/delfini.jpg'
         ],
         indexPhoto: 0, 
+    },    // avanti e inditro delle foto 
+    methods: {
+        prevPhoto() {
+            this.indexPhoto -= 1;
+            
+            if(this.indexPhoto < 0) {
+                this.indexPhoto = this.photos.length - 1  
+            }
+        },
+        nextPhoto() {
+            this.indexPhoto += 1;
+
+            if(this.indexPhoto > (this.photos.length - 1)) {
+                this.indexPhoto = 0;
+            }
+        }
     }
 
 });
